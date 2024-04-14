300745465fd6e37bf11642af2479424d6f116d33
FirasCh3 <firaschebil61@gmail.com>
2024-04-14 15:49:40 +0100

    changed file structure and connected to db

---
+++ BackEnd/Catalog/Catalog.php
@@ -0,0 +1,9 @@
+<?php
+   require_once "../connexion.php";
+   if ($conn){
+      echo "connected";
+   }else{
+      echo "failure";   
+   }
+
+?>
--- FrontEnd/Catalog/Script.js
+++ FrontEnd/Catalog/Script.js
@@ -2,7 +2,7 @@ function getParams(){
     const urlParams = new URLSearchParams(window.location.search);
     const myParam = urlParams.get('catalogId');
     query = new XMLHttpRequest()
-    query.open("GET", "//127.0.0.1/Terrai.net/FrontEnd/Catalog/Catalog.php")
+    query.open("GET", "//127.0.0.1/Terrai.net/BackEnd/Catalog/Catalog.php")
     query.send()
     query.onload=function(){
         console.log(this.responseText)
