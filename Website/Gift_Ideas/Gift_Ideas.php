57b427c30638cad0b5d3005e6c4db9e36b70c90a
khalilbahri <khalilbahri10@gmail.com>
2024-04-29 21:26:34 +0200

    t

--- Website/Gift_Ideas/Gift_Ideas.php
+++ Website/Gift_Ideas/Gift_Ideas.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="Style_GIFT.css">
+    <link rel="stylesheet" href="Style_GIFTS.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Abhaya+Libre:wght@400;500;600;700;800&display=swap" rel="stylesheet">
@@ -15,29 +15,28 @@
         <p>GIFT IDEAS</p>
         <div class='line'></div>
     </header>
+        <div class="row_GIFT">
+            <?php
+                require_once "../connexion.php"; 
+                $query = "select * from products where type_produit = 'coffret'";
+                $result = mysqli_query($conn, $query);
+                while($row = mysqli_fetch_assoc($result)){
+                    echo "<div class='col_GIFT'>
+                            <div class='content_GIFT'>
+                                <img src='$row[Product_image]'>
+                                <span></span>
+                                <button><a href=./Order/Order.php?productId=$row[Product_id]>Order now<a></button>
+                            </div>
+                            <div class='content-text_GIFT'>
+                                <p>$row[Product_name]</p>
+                                <p>$row[Product_price]$</p>
+                            </div>
+                        </div>";
+                }
 
-        <?php
-            require_once "../connexion.php"; 
-            $query = "select * from products where type_produit = 'coffret'";
-            $result = mysqli_query($conn, $query);
-            while($row = mysqli_fetch_assoc($result)){
-                echo "<div class='col_GIFT'>
-                        <div class='content_GIFT'>
-                            <img src='$row[Product_image]'>
-                            <span></span>
-                            <button><a href=./Order/Order.php?productId=$row[Product_id]>Order now<a></button>
-                        </div>
-                        <div class='content-text_GIFT'>
-                            <p>$row[Product_name]</p>
-                            <p>$row[Product_price]$</p>
-                        </div>
-                    </div>";
-            }
-
-            mysqli_close($conn);    
-        ?>
-
-    </div>
+                mysqli_close($conn);    
+            ?>
+        </div>
 
     <div class='customize'>
         <h1>Customize yours</h1>
