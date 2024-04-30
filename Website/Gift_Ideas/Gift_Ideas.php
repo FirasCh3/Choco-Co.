2a08f99111d179a89df3eb1755cdcf57e13879a6
FirasCh3 <firaschebil61@gmail.com>
2024-04-30 22:22:54 +0100

    test

--- Website/Gift_Ideas/Gift_Ideas.php
+++ Website/Gift_Ideas/Gift_Ideas.php
@@ -17,20 +17,7 @@
     <title>Gift Ideas</title>
 </head>
 <body>
-    <nav class="navbar">
-        <h1 class="logo">Choco Co.</h1>
-        <ul class="nav-links">
-             <li><a href="#Home" class="active">Home</a></li>
-             <li><a href="#Catalog">Catalog</a></li>
-             <li><a href="#Blog">Blog</a></li>
-             <li><a href="#Choco Co.">Choco Co.</a></li>
-         </ul>
-         <div class="icons">
-            <i href="#shooping" class="fa fa-shopping-cart"></i>
-            <i href="#search" class="fa fa-search"></i>
-         </div>
-         
-    </nav>
+    
 
     <header>
         <!--navbar-->
@@ -47,7 +34,7 @@
                             <div class='content_GIFT'>
                                 <img src='$row[Product_image]'>
                                 <span></span>
-                                <button><a href=./Order/Order.php?productId=$row[Product_id]>Order now<a></button>
+                                <button><a href=../Order/Order.php?productId=$row[Product_id]>Order now</a></button>
                             </div>
                             <div class='content-text_GIFT'>
                                 <p>$row[Product_name]</p>
@@ -66,9 +53,77 @@
         <span></span>
     </div>
 
-    </section>
         <!--
             footer
         -->
+    <footer class="services">
+    <section id="services">
+        <div class="contactus_et_map">
+            <div class="contactus">
+                <div class="description">
+                    <i class="fa fa-map-marker"></i>
+                    <span>Lorem ipsum dolor sit</span>
+                    <i class="fa fa-phone"></i>
+                    <span>(+216) 999 999 999</span>
+                    <h3>FOLLOW US</h3>
+                    <i class="fa fa-facebook"></i>
+                    <i class="fa fa-instagram"></i>
+                    <i class="fa fa-linkedin"></i>
+                </div>
+                <h1>Contact Us</h1>
+                <div class="formulaire">    
+                    <form action="">
+                        <h2>Leave us a message</h2>
+                        <input type="text" placeholder="Name" required>
+                        <input type="email" placeholder="Mail" required>
+                        <textarea placeholder ="Message"></textarea>
+                        <input type="submit" value="Send">
+                    </form>
+                </div>
+            </div>
+            <div class="col" id="maps">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2525.732494393068!2d-3.536356214648855!3d50.7249017985369!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x486da43958c82c81%3A0x3e47e71aceb7a4f3!2sChococo%20Exeter!5e0!3m2!1sen!2stn!4v1713897868474!5m2!1sen!2stn" width="94%" height=400em style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+    </section>
+    <div class="row">
+        <div class="col">
+            <img src=".\images\target.png" alt="">
+            <h4>Click & Collect</h4>
+            <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
+        </div>
+        <hr>
+        <div class="col">
+            <img src=".\images\delivery-truck.png" alt="">
+            <h4>Fast Delivery</h4>
+            <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>  
+        </div>
+        <hr>
+        <div class="col" id="gift">
+            <img src=".\images\gift.png" alt="">
+            <h4>Personalized Gifts</h4>
+            <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
+        
+        </div>
+        <hr>
+        <div class="col">
+            <img src=".\images\verified.png" alt="">
+            <h4>Secure Payment</h4>
+            <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
+            
+        </div>
+        <hr>
+        <div class="col">
+            <img src=".\images\swap.png" alt="">
+            <h4>Free Return</h4>
+            <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
+        </div>
+
+    </div>
+
+    <!-- Droits d'auteur -->
+    <div class="bas">&copy; Choco Co. -2024</div> 
+      
+</footer>
 </body>
 </html>
