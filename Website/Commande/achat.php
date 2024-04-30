5028768b901cb2f5a0fa1d3f19b6d9b5c30586f1
FirasCh3 <firaschebil61@gmail.com>
2024-04-30 11:12:44 +0100

    finished commande page

---
+++ Website/Commande/achat.php
@@ -0,0 +1,11 @@
+<?php
+    session_start();
+    $firstname = trim($_POST["firstname"]);
+    $lastname = trim($_POST["lastname"]);
+    $email = trim($_POST["email"]);
+    $number = trim($_POST["number"]);
+    $zip = trim($_POST["zip"]);
+    $city = trim($_POST["city"]);
+    $state = trim($_POST["address"]);
+    
+?>
