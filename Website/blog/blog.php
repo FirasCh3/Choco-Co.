c312dbcaf045a146af5742e76591a066ae01c6c0
Ameni-BESBES <besbesameni2003@gmail.com>
2024-05-02 21:05:14 +0100

    codecarousel

--- Website/blog/blog.php
+++ Website/blog/blog.php
@@ -5,14 +5,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Catalog</title>
-    <script src="Script.js"></script>
+    <title>Blog</title>
     <link href="styles.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css2?family=Abhaya+Libre:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <script src="script.js"></script>
 </head>
-<body>
+<body>                                                       
     <header>
         <!--navbar-->
         <p class="title">BLOG</p>
@@ -47,54 +47,24 @@
     </div>
 
 
-
-    <div id="hero-carousel" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-        <div class="carousel-item active c-item">
-            <img src="https://images.unsplash.com/photo-1481391319762-47dff72954d9?q=80&w=1530&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="d-block w-100 c-img" alt="Slide 1">
-            <div class="carousel-caption top-0 mt-4">
-            <p class="mt-5 fs-3 text-uppercase">New Recipe!</p>
-            <h1 class="display-1 fw-bolder text-capitalize">Chocolate with Strawberry</h1>
-            <button class="btn btn-secondary px-4 py-2 fs-5 mt-5">Discover</button>
-            </div>
-        </div>
-        <div class="carousel-item c-item">
-            <img src="https://media.istockphoto.com/id/488182109/photo/chocolates.jpg?s=612x612&w=0&k=20&c=OAzD-G8GZZfzb5bgZ5CJd_PdSIJqdnInLmEeeiDGRe0=" class="d-block w-100 c-img" alt="Slide 2">
-            <div class="carousel-caption top-0 mt-4">
-            <p class="text-uppercase fs-3 mt-5">New Recipe!</p>
-            <p class="display-1 fw-bolder text-capitalize">Chocolate Pie</p>
-            <button class="btn btn-secondary px-4 py-2 fs-5 mt-5" data-bs-toggle="modal"
-                data-bs-target="#booking-modal">Discover</button>
-            </div>
-        </div>
-        <div class="carousel-item c-item">
-            <img src="https://media.istockphoto.com/id/627574168/photo/gift-of-homemade-fudge.jpg?s=612x612&w=0&k=20&c=IBPbNAZxf1FKvyfOTrcpIpzf2cVyAM14AtIOsBOWiDs=" class="d-block w-100 c-img" alt="Slide 3">
-            <div class="carousel-caption top-0 mt-4">
-            <p class="text-uppercase fs-3 mt-5">New Recipe!</p>
-            <p class="display-1 fw-bolder text-capitalize">Mousse with Chocolate</p>
-            <button class="btn btn-secondary px-4 py-2 fs-5 mt-5" data-bs-toggle="modal"
-                data-bs-target="#booking-modal">Discover</button>
-            </div>
-        </div>
-
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#hero-carousel" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#hero-carousel" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-        </button>
-
-
-
-
-
-
+    <div class="carousel" duration="7000">
+    <ul tabindex="0">
+        <li id="c1_slide1"><div>Gift Chocolate!<br /><a href="https://www.google.com">See product</a></div></li>  
+        <li id="c1_slide2"><div>Gift Chocolate!<br /><a href="https://www.google.com">See product</a></div></li>  
+        <li id="c1_slide3"><div>Gift Chocolate!<br /><a href="https://www.google.com">See product</a></div></li>  
+        <li id="c1_slide4"><div>Gift Chocolate!<br /><a href="https://www.google.com">See product</a></div></li>  
+    </ul>
+    <ol>
+        <li><a href="#c1_slide1"></a></li>
+        <li><a href="#c1_slide2"></a></li>
+        <li><a href="#c1_slide3"></a></li>
+        <li><a href="#c1_slide4"></a></li>
+    </ol>
+    <div class="prev">&lsaquo;</div>
+    <div class="next">&rsaquo;</div>
     </div>
-   
 
-        <!--footer-->
+
+
 </body>
 </html>
