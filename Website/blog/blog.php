581c7e4657f9c9408e0354eca56472d21afe7169
FirasCh3 <firaschebil61@gmail.com>
2024-05-02 22:05:22 +0100

    changes

--- Website/blog/blog.php
+++ Website/blog/blog.php
@@ -10,7 +10,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog</title>
     <link href="styles.css" rel="stylesheet">
-    
     <link href="https://fonts.googleapis.com/css2?family=Abhaya+Libre:wght@400;500;600;700&display=swap" rel="stylesheet">
 
     <script src="script.js"></script>
