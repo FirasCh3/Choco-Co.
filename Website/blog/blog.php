b97c120123c1d017d698995ba93e0921cdd93117
Ameni-BESBES <besbesameni2003@gmail.com>
2024-05-02 21:45:39 +0100

    test

--- Website/blog/blog.php
+++ Website/blog/blog.php
@@ -10,11 +10,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css2?family=Abhaya+Libre:wght@400;500;600;700&display=swap" rel="stylesheet">
-<<<<<<< HEAD
+
     <script src="script.js"></script>
 </head>
 <body>                                                       
-=======
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -25,7 +25,7 @@
 
 
 <body>
->>>>>>> 8690fef93434892d00d842212f384ebc1fecd11e
+
     <header>
         <!--navbar-->
         <nav class="navbar">
@@ -43,11 +43,6 @@
          </div>
          
         </nav>
-
-
-
-
-
 
         <p class="title">BLOG</p>
         <p class="sub-title">Recipes & Gift Ideas</p>
